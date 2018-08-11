79ae16f830ddd3b3f69cf7f5732dc59342a0287b
Grzegorz Klimaszewski <grzegorz.klimaszewski@yahoo.com>
2018-08-11 13:51:04 +0100

    LogFilters._update_hash simplified and made more generic

--- clog.rs
+++ clog.rs
@@ -2,8 +2,15 @@ use std::io::{self, BufRead};
 use std::collections::HashMap;
 
 struct LogFilters {
-    // Each vector line stores a vector of individual words and wildcards
-    line_filters: Vec<Vec<String>>,
+    // Each vector line stores a vector of individual words variations
+    // line_filters (Vec)
+    //    |
+    //    |- words (Vec)
+    //          |
+    //          |- word_variations (Vec)
+    //                   |
+    //                   |- word (String)
+    line_filters: Vec<Vec<Vec<String>>>,
     // Each unique word from `line_filters` gets its own key
     // Each key stores references to lines containing the key
     words_hash: HashMap<String, Vec<u32>>
@@ -20,45 +27,75 @@ impl LogFilters {
         }
     }
 
-    fn _update_hash(&mut self) {
-        let last_filter_index = self.line_filters.len() as u32 - 1;
-        let last_filter = self.line_filters.last();
-        if last_filter.is_some() {
-            let last_filter = last_filter.unwrap();
-            for word in last_filter {
-                self.words_hash.entry(word.clone()).or_insert(vec![last_filter_index]);
-                let mut vector_indexes = self.words_hash.get_mut(word).unwrap();
-                if *vector_indexes.last().unwrap() != last_filter_index {
-                    vector_indexes.push(last_filter_index);
-                }
-            }
-        }
+    fn _update_hash(&mut self, word: &String, filter_index: u32) {
+        self.words_hash.entry(word.clone()).or_insert(vec![filter_index]);
+        let mut vector_indexes = self.words_hash.get_mut(word).unwrap();
+        vector_indexes.push(filter_index);
+        vector_indexes.sort();
+        vector_indexes.dedup();
     }
 
     fn _add_to_filters(&mut self, log_line: &str) {
-        let words_iterator = log_line.split(|c| c == ' ' || c == '/' || c == ',' || c == '.');
+        let words_iterator = log_line.split(|c|
+            c == ' ' ||
+            c == '/' ||
+            c == ',' ||
+            c == '.' ||
+            c == ':' ||
+            c == '"' ||
+            c == '(' ||
+            c == ')' ||
+            c == '[' ||
+            c == ']');
         let mut words = Vec::new();
+        let expected_index = self.line_filters.len() as u32;
 
         for word in words_iterator {
             let word = word.to_string();
-            words.push(word);
+            if word.len() > 0 {
+                self._update_hash(&word, expected_index);
+
+                let word_variations = vec![word];
+                words.push(word_variations);
+            }
         }
 
         self.line_filters.push(words);
     }
 
+    fn _find_best_match(min_consequent_matches: u32) {
+        // TODO
+    }
+
     fn learn_line(&mut self, log_line: &str) {
         self._add_to_filters(log_line);
-        self._update_hash();
+    }
+
+    fn save_filters(self){
+        // TODO
+    }
+
+    fn load_filters(self){
+        // TODO
     }
 
     fn print(self) {
-        for elem in self.line_filters {
-            println!("{:?}", elem);
+        if self.line_filters.len() > 0 {
+            for elem in self.line_filters {
+                println!("{:?}", elem);
+            }
+        }
+        else {
+            println!("No filters added yet");
         }
         println!();
-        for (key, value) in self.words_hash {
-            println!("{} : {:?}", key, value);
+        if self.words_hash.len() > 0 {
+            for (key, value) in self.words_hash {
+                println!("{} : {:?}", key, value);
+            }
+        }
+        else {
+            println!("No words with references to filters added yet");
         }
     }
 }
@@ -71,7 +108,7 @@ fn main() {
         let log_line = line.expect("INVALID INPUT!");
         log_filters.learn_line(&log_line);
     }
-    
+
     log_filters.print();
 }
 
