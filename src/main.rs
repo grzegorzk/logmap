c5fb0a0622dc48406b1b6051fcfddb6c21df2f3f
Grzegorz Klimaszewski <grzegorz.klimaszewski@yahoo.com>
2018-08-26 23:14:24 +0100

    Minor change to main - allowing two new alternatives.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod clog;
 fn main() {
     let std_in = io::stdin();
     let mut log_filters = clog::LogFilters::new();
+    log_filters.max_allowed_new_alternatives = 2;
 
     let mut icnt = 0;
     for line in std_in.lock().lines() {
         let log_line = line.expect("INVALID INPUT!");
         log_filters.learn_line(&log_line);
+        // Debug to help assessing performance
         icnt += 1;
         if icnt % 1000 == 0 {
             eprintln!("{}", icnt);
