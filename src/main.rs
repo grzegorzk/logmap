c94438c0d4932d108345ed4a50539dfef616bf4d
Grzegorz Klimaszewski <grzegorz.klimaszewski@yahoo.com>
2018-09-17 23:53:44 +0100

    Adding base implementation of `main`.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
+extern crate getopts;
+
 use std::io::{self, BufRead};
+use std::process::exit;
+use std::path::Path;
+use std::env;
+
 mod clog;
 
-fn main() {
-    let std_in = io::stdin();
-    let mut log_filters = clog::LogFilters::new();
-    log_filters.max_allowed_new_alternatives = 2;
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut opts = getopts::Options::new();
 
-    let mut icnt = 0;
-    for line in std_in.lock().lines() {
-        let log_line = line.expect("INVALID INPUT!");
-        log_filters.learn_line(&log_line);
-        // Debug to help assessing performance
-        icnt += 1;
-        if icnt % 1000 == 0 {
-            eprintln!("{}", icnt);
+    opts.optopt("l", "load", "Load filters from given path and use to scan logs from input", "PATH");
+    opts.optopt("s", "save", "Save filters under given path, does not work when piping", "PATH");
+    opts.optflag("m", "map", "Map filters from input (extend already loaded filters if -l was used)");
+    opts.optflag("h", "help", "Print this help menu");
+
+    let matches = match opts.parse(&args) {
+        Ok(_option) => _option,
+        Err(_) => {
+            println!("{}", opts.usage(""));
+            exit(1);
         }
+    };
+
+    if matches.opt_present("h") {
+        println!("{}", opts.usage(""));
+        exit(0);
     }
 
-    log_filters.print();
-}
+    let mut log_filters = clog::LogFilters::new();
+    log_filters.max_allowed_new_alternatives = 2;
+    if matches.opt_str("l").is_some() {
+        let file_path_str = matches.opt_str("l").unwrap();
+        let load_file_path = Path::new(&file_path_str);
+        log_filters = clog::LogFilters::load(load_file_path);
+    }
+    if matches.opt_present("m") {
+        let std_in = io::stdin();
+        let mut icnt = 0;
+        for line in std_in.lock().lines() {
+            let log_line = line.expect("INVALID INPUT!");
+            log_filters.learn_line(&log_line);
 
+            // Debug to help assessing performance
+            icnt += 1;
+            if icnt % 10000 == 0 {
+                eprintln!("Already processed {} lines.", icnt);
+            }
+        }
+    }
+    if matches.opt_str("s").is_some() {
+        let file_path_str = matches.opt_str("s").unwrap();
+        let save_file_path = Path::new(&file_path_str);
+        log_filters.save(&save_file_path);
+    }
+
+    exit(0);
+}
