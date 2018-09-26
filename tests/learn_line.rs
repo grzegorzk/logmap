8ed40cfd4fa0f8fea4afb027dc4a542590ca4718
Grzegorz Klimaszewski <grzegorz.klimaszewski@yahoo.com>
2018-09-26 03:00:40 +0100

    Further simplification to log file format.

--- tests/learn_line.rs
+++ tests/learn_line.rs
@@ -2,6 +2,16 @@ extern crate logmap;
 
 #[test]
 fn no_alternatives_allowed() {
+    let mut log_filters = logmap::logmap::LogFilters::new();
+    log_filters.max_allowed_new_alternatives = 1;
+    log_filters.min_req_consequent_matches = 3;
+    log_filters.ignore_numeric_words = true;
+    log_filters.ignore_first_columns = 2;
+    
+    log_filters.learn_line("Sep 16 20:17:04 AM kernel: wlp2s0: authenticated");
+    let expected = "[kernel],[wlp2s0],[authenticated]";
+    assert_eq!(log_filters.to_string(), expected);
+    println!("{}", log_filters.to_string());
 }
 
 #[test]
